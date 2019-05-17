6d0eae211291987f75c50e4331a7e69faf3f0fa5
foloke <foloke@yandex.ru>
2019-05-17 17:36:07 +0300

    SaveManager class

---
+++ app/src/main/java/com/sb9/foloke/sectorb9/game/Managers/SaveManager.java
@@ -0,0 +1,27 @@
+package com.sb9.foloke.sectorb9.game.Managers;
+
+public class SaveManager
+{
+	private class LoadThread extends Thread
+	{
+		public LoadThread()
+		{
+			
+		}
+
+		@Override
+		public void run()
+		{
+			// TODO: Implement this method
+			super.run();
+		}
+		
+		
+	}
+	//TODO:
+	public void load()
+	{}
+	public void save()
+	{}
+	
+}
